894130cc28d2f2495f8a71f50dbefac77d4b82dd
Daniel <dany19992212@gmail.com>
2025-10-30 00:20:10 -0500

    agregar configuración de conexión a la base de datos y variables de entorno

--- models/server.js
+++ models/server.js
@@ -1,15 +1,26 @@
 const express = require("express");
 const cors = require("cors");
 
+const { dbConnection } = require("../database/config");
+
 class Server {
   constructor() {
     this.app = express();
     this.port = process.env.PORT || 3000;
     this.usuariosPath = "/api/usuarios";
 
+    //conectar a base de datos
+    this.conectarDB();
+
+    //middlewares
     this.middelwares();
 
+    //rutas de mi aplicacion
     this.routes();
+  }
+
+  async conectarDB() {
+    await dbConnection();
   }
 
   middelwares() {
