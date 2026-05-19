2fc0d98cc34e5076213356cfcd291c6a4725b366
Daniel <dany19992212@gmail.com>
2026-05-18 23:54:10 -0500

    probado validaciones de roles en usuarios

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,6 @@
 const { Router } = require("express");
 const { check } = require("express-validator");
+const Role = require("../models/role");
 
 const { validatedFields } = require("../middlewares/validate_fields");
 
@@ -27,7 +28,13 @@ router.post("/",[
   check("name", "El nombre es obligatorio").not().isEmpty(),
   check("email", "El correo es obligatorio").isEmail(),
   check("password", "La contraseña debe de ser más de 6 letras").isLength({ min: 6 }),
-  check("rol", "No es un rol válido").isIn(["ADMIN_ROLE", "USER_ROLE"]),
+  //check("rol", "No es un rol válido").isIn(["ADMIN_ROLE", "USER_ROLE"]),
+  check("rol").custom(async (rol = '') => {
+    const existeRol = await Role.findOne({ rol });
+    if (!existeRol) {
+      throw new Error(`El rol ${rol} no está registrado en la BD`);
+    }
+  }),
   validatedFields
 ], useriosPost);
 
