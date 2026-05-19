64ba6dec491a99896ec0c763ceb0d21e17ea3dda
Daniel <dany19992212@gmail.com>
2026-05-19 18:16:44 -0500

    creo la carpeta de helpers y se creo db-validators.js

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,8 @@
 const { Router } = require("express");
 const { check } = require("express-validator");
-const Role = require("../models/role");
 
 const { validatedFields } = require("../middlewares/validate_fields");
+const { esRoleValido } = require("../helpers/db-validators");
 
 const {
   useriosGet,
@@ -11,6 +11,7 @@ const {
   useriosDelete,
   useriosPatch,
 } = require("../controllers/user");
+
 const router = Router();
 
 router.get(
@@ -29,12 +30,7 @@ router.post("/",[
   check("email", "El correo es obligatorio").isEmail(),
   check("password", "La contraseña debe de ser más de 6 letras").isLength({ min: 6 }),
   //check("rol", "No es un rol válido").isIn(["ADMIN_ROLE", "USER_ROLE"]),
-  check("rol").custom(async (rol = '') => {
-    const existeRol = await Role.findOne({ rol });
-    if (!existeRol) {
-      throw new Error(`El rol ${rol} no está registrado en la BD`);
-    }
-  }),
+  check("rol").custom(esRoleValido),
   validatedFields
 ], useriosPost);
 
