f0e9d33c029c88a8ba3dab5a74c1bbfba9fceb95
Daniel <dany19992212@gmail.com>
2026-07-15 21:57:14 -0500

    se implementaron funciones de búsqueda para usuarios, categorías y productos; se mejoró la lógica de búsqueda en el controlador

--- controllers/search.js
+++ controllers/search.js
@@ -1,9 +1,71 @@
 const { response } = require("express");
+const { ObjectId } = require("mongoose").Types;
+
+const { User, Category, Product } = require("../models");
 
 const collectionsAllowed = ["users", "categories", "products", "roles"];
 
 const findUsers = async (term = "", res = response) => {
   const isMongoId = ObjectId.isValid(term);
+
+  if (isMongoId) {
+    const user = await User.findById(term);
+    return res.json({
+      results: user ? [user] : [],
+    });
+  }
+
+  const regex = new RegExp(term, "i");
+
+  const users = await User.find({
+    $or: [{ name: regex }, { email: regex }],
+    $and: [{ estado: true }],
+  });
+
+  return res.json({
+    results: users,
+  });
+};
+
+const findCategories = async (term = "", res = response) => {
+  const isMongoId = ObjectId.isValid(term);
+
+  if (isMongoId) {
+    const category = await Category.findById(term);
+    return res.json({
+      results: category ? [category] : [],
+    });
+  }
+
+  const regex = new RegExp(term, "i");
+
+  const categories = await Category.find({ name: regex, estado: true });
+
+  return res.json({
+    results: categories,
+  });
+};
+
+const findProducts = async (term = "", res = response) => {
+  const isMongoId = ObjectId.isValid(term);
+
+  if (isMongoId) {
+    const product = await Product.findById(term);
+    return res.json({
+      results: product ? [product] : [],
+    });
+  }
+
+  const regex = new RegExp(term, "i");
+
+  const products = await Product.find({ name: regex, estado: true }).populate(
+    "category",
+    "name",
+  );
+
+  return res.json({
+    results: products,
+  });
 };
 
 const search = async (req, res = response) => {
@@ -17,24 +79,19 @@ const search = async (req, res = response) => {
 
   switch (collection) {
     case "users":
-      break;
+      return await findUsers(term, res);
 
     case "categories":
-      break;
+      return await findCategories(term, res);
 
     case "products":
-      break;
+      return await findProducts(term, res);
 
     default:
       return res.status(500).json({
         msg: "Se me olvido hacer esta busqueda",
       });
   }
-
-  res.json({
-    collection,
-    term,
-  });
 };
 
 module.exports = {
